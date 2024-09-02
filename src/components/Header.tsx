a6ce16b95b369e3c82dff93054f673e68250f713
Nicolas <rochmar.nicolas@accenture.com>
2024-09-02 15:35:07 +0800

    Add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+export default function Header() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
 
 export default function MainLayout() {
   return (
     <div>
-      Layout
+      <Header></Header>
       <Outlet></Outlet>
     </div>
   );
