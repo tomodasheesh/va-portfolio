bb4124b2931ccd6c04bb1fc9cd9ed9c2487d53ff
Nicolas <rochmar.nicolas@accenture.com>
2024-09-04 11:34:34 +0800

    init home text

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { purple, pink } from '@mui/material/colors';
+import { purple } from '@mui/material/colors';
 
 import { PageRouteProvider } from './router/provider';
 
@@ -11,7 +11,9 @@ import './index.css';
 const theme = createTheme({
   palette: {
     primary: purple,
-    secondary: pink,
+    secondary: {
+      main: '#ee99fc'
+    },
   },
 });
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,7 @@
+import Grid from '@mui/material/Grid2';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -5,9 +9,47 @@ export default function Home() {
     <div className={styles.home}>
       <div className={styles.home_bg_image} />
       
-      <div className={styles.home_text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam odio aliquam autem, quisquam, asperiores tempore ex molestias ut voluptates aperiam, quas facere deleniti! Nihil esse distinctio eligendi dignissimos voluptates eaque.
-      </div>
+      <Grid 
+        container
+        className={`h-full centered ${styles.home_text_container}`}
+      >
+        <Grid size={{
+          xs: 11,
+          md: 9
+        }}>
+          <Box sx={{
+            fontWeight: 'bold',
+            fontSize: {
+              xs: '28px',
+              md: '32px'
+            }
+          }}>
+            Necessitatibus cum quae officiis officia tenetur dolor vero harum aut iste delectus.
+          </Box>
+          <Box sx={{
+            fontSize: {
+              xs: '18px',
+              md: '24px'
+            }
+          }}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, laudantium, recusandae eius doloribus aliquid fugit odio voluptate debitis sunt aspernatur, quia aut aperiam commodi magnam incidunt alias porro quisquam possimus.
+          </Box>
+
+          <Box sx={{
+            marginTop: '24px',
+            display: 'flex',
+            gap: 1
+          }}>
+            <Button variant='outlined' color='secondary'>
+              Button 1
+            </Button>
+
+            <Button variant='contained'>
+              Button 2
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
     </div>
   );
 }
